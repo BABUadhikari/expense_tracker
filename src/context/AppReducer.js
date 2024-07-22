1fb8642eb0d77b5a487931e0ed44807edbebda4b
Baburam <psnbabu5@gmail.com>
2024-07-22 12:03:30 +0545

    add delete transaction

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,6 +1,15 @@
-export default (state, action) => {
+const AppReducer = (state, action) => {
   switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
 };
+
+export default AppReducer;
