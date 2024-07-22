90f1a350a3f069a6df940f7b3b1bc94bd36f0953
Baburam <psnbabu5@gmail.com>
2024-07-22 11:12:47 +0545

    add out going and incoming balance functionality and list in income and expense

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 function IncomeExpense() {
+  const { transactions } = useContext(GlobalContext);
+  const incomeBalance = transactions
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+  const expenseBalance = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
   return (
-    <div className='flex h-20 mt-6 justify-around items-center bg-slate-200 shadow-xl rounded-lg'>
-      <div >
+    <div className="flex h-20 mt-4 justify-around items-center bg-slate-200 shadow-xl rounded-lg">
+      <div>
         <h1>Income</h1>
-        <p className='text-green-600'>+$0.00</p>
+        <p className="text-green-600">+${incomeBalance}</p>
       </div>
       <div>
         <h1>Expense</h1>
-        <p className='text-red-600'>-$0.00</p>
+        <p className="text-red-600">-${Math.abs(expenseBalance)}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default IncomeExpense
+export default IncomeExpense;
