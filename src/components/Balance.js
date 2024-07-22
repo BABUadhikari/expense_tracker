ddb684a4f952c436ee17b12a1f8ed22fa3e65cc5
Baburam <psnbabu5@gmail.com>
2024-07-22 11:10:21 +0545

    add total balance functionality

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 function Balance() {
+  const { transactions } = useContext(GlobalContext);
+
+  const totalBalance = transactions.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
   return (
-    <div className='flex flex-col justify-start mt-2'>
-      
-        <h4> Your Balance</h4>
-        <h1 className='text-2xl'> $ 0.00 </h1>
-      
+    <div className="flex flex-col justify-start mt-2">
+      <h4> Your Balance</h4>
+      <h1 className="text-2xl">
+        {totalBalance < 0 ? "-" : "+"}${Math.abs(totalBalance)}{" "}
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Balance
+export default Balance;
